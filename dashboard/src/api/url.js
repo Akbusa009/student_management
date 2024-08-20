96acbacc1151094eb5fa5e3aeab16bce283b0151
akshay busa <akshaybusa009@gmail.com>
2024-08-20 13:00:50 +0530

    first commit

---
+++ dashboard/src/api/url.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://student-management-xi-eight.vercel.app/"
+// export const BASE_URL = "http://localhost:4000";
+// export const BASE_URL = "https://precious-cyan-moccasins.cyclic.app";
